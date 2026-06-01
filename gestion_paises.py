cf9769d69b5e3124c2ba6b25cd534423302636e6
gonzzacar <gonzaloelmaspro5@gmail.com>
2026-06-01 16:40:54 -0300

    Segundo commit

--- gestion_paises.py
+++ gestion_paises.py
@@ -1,3 +1,33 @@
+from funciones_principales import *
+import csv
+import os
+ARCHIVO_CSV = 'paises.csv'
+def cargar_paises():
+    paises = []
+
+    try:
+        with open(ARCHIVO_CSV, mode="r", encoding="utf-8") as archivo:
+            lector = csv.DictReader(archivo)
+
+            for fila in lector:
+                try:
+                    pais = {
+                        "nombre": fila["nombre"],
+                        "poblacion": int(fila["poblacion"]),
+                        "superficie": int(fila["superficie"]),
+                        "continente": fila["continente"]
+                    }
+
+                    paises.append(pais)
+
+                except:
+                    print("Error en una fila del CSV.")
+
+    except FileNotFoundError:
+        print("Archivo CSV no encontrado. Se creará uno nuevo.")
+
+    return paises
+
 def pedir_texto(mensaje):
     while True:
         texto = input(mensaje).strip()
