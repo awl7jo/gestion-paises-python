2be9b6d458537173649afbcd3d01431c4d8014c3
awl7jo <alejorei06122@gmail.com>
2026-06-08 16:39:59 -0300

    filtrar paises

--- funciones_principales.py
+++ funciones_principales.py
@@ -1,19 +1,24 @@
 
-def filtrar_continente(paises):
+def filtrar_paises(paises):
+    print('\n--- FILTRAR PAISES ---')
     try:
-        continente = input("Ingresá el continente: ").strip()
-        if not continente:
-            raise ValueError("El continente no puede estar vacío.")
+        if not paises:
+            raise ValueError('No hay paises cargados')
+        print('1. Por continente')
+        print('2. Por rango de poblacion')
+        print('3. Por rango de superficie')
+        print('0. Volver al menu principal')
+        opcion = int(input('Ingrese una opcion ')).strip()
 
-        filtrados = []
-        for pais in paises:
-            if pais["continente"].lower() == continente.lower():
-                filtrados.append(pais)
+        opciones = {'1': filtrar_continente,
+                    '2': filtrar_por_poblacion,
+                    '3': filtrar_por_superficie
 
-        if len(filtrados) == 0:
-            raise ValueError(f"No se encontraron países en '{continente}'.")
-
-        mostrar_paises(filtrados)
-
+                    }
+        if opcion == '0':
+            return
+        if opcion not in opciones:
+            raise ValueError('Opcion invalida. ingresa un numero del 0 al 3.')
+        opciones[opcion](paises)
     except ValueError as e:
-        print(f"Error: {e}")
+        print(f'Error: {e}')
